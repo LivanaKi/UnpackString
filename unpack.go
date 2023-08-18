fc6dcee2ff303de4e1bbfd156a4204ffb06c06c7
LivanaKi <natzasha2015@gmail.com>
2023-08-18 16:21:14 +0300

    :|

---
+++ unpack.go
@@ -0,0 +1,41 @@
+package hw02unpackstring
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+var ErrInvalidString = errors.New("invalid string")
+
+func Unpack(start string) (string, error) {
+
+	var result strings.Builder
+
+	if len(start) == 0 {
+		return "", nil
+	}
+	for i, solution := range start {
+		if unicode.IsNumber(rune(start[0])) {
+			return "", ErrInvalidString
+		}
+		if unicode.IsDigit(solution) {
+			num, _ := strconv.Atoi(string(start[i]))
+			if num == 1 && unicode.IsDigit(rune(start[i+1])) {
+				num1, _ := strconv.Atoi(string(start[i+1]))
+				if num1 == 0 {
+					return "", ErrInvalidString
+				}
+			}
+			if num == 0 {
+				return "", ErrInvalidString
+			}
+			result.WriteString(strings.Repeat(string(start[i-1]), num-1))
+		}
+		if unicode.IsLetter(solution) {
+			result.WriteRune(solution)
+		}
+	}
+	return result.String(), nil
+}
---
+++ unpack_test.go
@@ -0,0 +1,56 @@
+package hw02unpackstring
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type test struct {
+	input    string
+	expected string
+	err      error
+}
+
+func TestUnpack(t *testing.T) {
+	for _, tst := range [...]test{
+		{
+			input:    "a4bc2d5e",
+			expected: "aaaabccddddde",
+		},
+		{
+			input:    "abcd",
+			expected: "abcd",
+		},
+		{
+			input:    "45",
+			expected: "",
+			err:      ErrInvalidString,
+		},
+		{
+			input:    "aaa10b",
+			expected: "",
+			err:      ErrInvalidString,
+		},
+		{
+			input:    "",
+			expected: "",
+		},
+		{
+			input:    "a2o3f0",
+			expected: "",
+			err:      ErrInvalidString,
+		},
+		{
+			input:    "a2c000",
+			expected: "",
+			err:      ErrInvalidString,
+		},
+	} {
+		t.Run(tst.input, func(t *testing.T) {
+			result, err := Unpack(tst.input)
+			require.Equal(t, tst.err, err)
+			require.Equal(t, tst.expected, result)
+		})
+	}
+}
